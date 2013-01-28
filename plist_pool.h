9a4458c303a4f27c07a25484984243efa7ba56c0
Kyle Maxwell <kyle@kylemaxwell.com>
2013-01-27 22:40:49 -0800

    versions

--- plist_pool.h
+++ plist_pool.h
@@ -4,6 +4,7 @@
 typedef struct {
 	char **buffer;
 	long capacity;
+	int min_version;
 } plist_pool;
 
 typedef struct {
@@ -14,6 +15,7 @@ typedef struct {
 typedef struct _plist_block {
 	struct _plist_block *prev;
 	struct _plist_block *next;
+	int prev_version;
 	int entries_count;
 	plist_entry entries[];
 } plist_block;
@@ -21,6 +23,7 @@ typedef struct _plist_block {
 typedef struct {
 	int block_count;
 	plist_block *last;
+	int last_version;
 } plist;
 
 plist_pool *
