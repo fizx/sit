cf11ca4897787c5d605ce3b66f911712c4186e4b
Kyle Maxwell <kyle@kylemaxwell.com>
2013-02-14 00:09:39 -0800

    add a test for a lot of input

--- spec/line_protocol_spec.rb
+++ spec/line_protocol_spec.rb
@@ -11,21 +11,27 @@ include Sit
 describe "integration" do
   before do
     @engine = Engine.new(Parser.new_json, 1_000_000)
- 		@input = Input.new(@engine, 1 << 10, 1 << 20)
+    @input = Input.new(@engine, 1 << 10, 1 << 20)
     @proto = LineProtocol.new(@input)
   end
   
-	Dir[File.dirname(__FILE__) + "/proto/*.in"].each do |path|
-		it "should run #{path} successfully" do
-			out = path.sub(".in", ".out")
-			File.exists?(out).should == true
-			@proto.consume(File.read(path))
-			expected = File.readlines(out)
-			actual = @proto.output
+  it "should be able to handle a lot of lines" do
+    1000.times do
+      @proto.consume("{\"hello\":\"world\"}\n" * 100)
+    end
+  end
+  
+  Dir[File.dirname(__FILE__) + "/proto/*.in"].each do |path|
+    it "should run #{path} successfully" do
+      out = path.sub(".in", ".out")
+      File.exists?(out).should == true
+      @proto.consume(File.read(path))
+      expected = File.readlines(out)
+      actual = @proto.output
       actual.zip(expected).each do |a,e|
         a.should == e.chomp
       end
       expected.size.should == actual.size
-		end
-	end
+    end
+  end
 end
