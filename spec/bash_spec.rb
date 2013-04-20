0a02352e06baeb69bb296a16fddf9a50a976ee5c
Kyle Maxwell <kyle@kylemaxwell.com>
2013-04-20 16:09:54 -0700

    fix test false positives

--- spec/bash_spec.rb
+++ spec/bash_spec.rb
@@ -13,7 +13,9 @@ describe "integration" do
 			out = `cd "#{File.dirname(path)}/../.." && cat #{path} | ./sit #{optstr} --test-mode 2>/dev/null`.strip
 			out.lines.count.should == expected.lines.count
 			expected.lines.zip(out.lines).each do |e, o|
-			  o.should =~ Regexp.compile(Regexp.escape(e).gsub(/[^\\]\\*/, ".*"))
+			  puts e
+			  puts Regexp.compile(Regexp.escape(e).gsub(/[^\\]\\*/, ".*"))
+			  o.should =~ Regexp.compile(Regexp.escape(e).gsub(/[^\\]\*/, ".*"))
 		  end
 		end
 	end
